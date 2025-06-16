21984f3f6532c434350504256907f44fefc7dff1
Margieva_Julia <ylmarg@mail.ru>
2025-06-16 21:13:56 +0300

    First Commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,34 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.common.exceptions import NoSuchElementException
 
-def login_saucedemo(username_value, password_value):
-    # Инициализация драйвера (предполагается, что у вас установлен ChromeDriver)
+def check_elements():
     driver = webdriver.Chrome()
 
     try:
         # Переход на страницу
         driver.get("https://www.saucedemo.com/")
 
-        # Находим поле для ввода имени пользователя
-        username_field = driver.find_element(By.ID, "user-name")
-        # Находим поле для ввода пароля
-        password_field = driver.find_element(By.ID, "password")
-        # Находим кнопку входа (submit)
-        submit_button = driver.find_element(By.ID, "login-button")
-
-        # Вводим данные
-        username_field.send_keys(username_value)
-        password_field.send_keys(password_value)
-
-        # Нажимаем кнопку
-        submit_button.click()
-
-        # Можно добавить задержку для проверки результата
-        time.sleep(3)
-
+        # Попытка найти элементы
+        try:
+            username_field = driver.find_element(By.ID, "user-name")
+            password_field = driver.find_element(By.ID, "password")
+            submit_button = driver.find_element(By.ID, "login-button")
+            # Если все элементы найдены, выводим сообщение
+            print("Элементы найдены")
+        except NoSuchElementException:
+            print("Один или несколько элементов не найдены")
     finally:
-        # Закрываем браузер
         driver.quit()
 
+# Вызов функции
+# check_elements()
